ad926a062e3a1e90248414b8d7effd063c82a1e7
HesselBouma <hesseltjeb@gmail.com>
2019-03-26 20:59:40 +0100

    Try

---
+++ ShooterTest/Assets/ButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public Animator WallAnimation;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("raak");
+            WallAnimation.enabled = true;
+        }
+    }
+
+
+}
--- ShooterTest/Assets/GunScript.cs
+++ ShooterTest/Assets/GunScript.cs
@@ -5,6 +5,7 @@ public class GunScript : MonoBehaviour{
 
     public float damage = 10f;
     public float range = 100f;
+    public GameObject ImpactEffect;
 
     public Camera fpsCam;
     public ParticleSystem MuzzleFlash;
@@ -25,13 +26,17 @@ public class GunScript : MonoBehaviour{
         RaycastHit hit;
        if( Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
-            Debug.Log(hit.transform.name);
+            Debug.Log(fpsCam.transform.position);
 
             Target target = hit.transform.GetComponent<Target>();
             if (target != null)
             {
                 target.TakeDamage(damage);
 
+            }
+            {
+                Object impactGO = Instantiate(ImpactEffect, hit.point, Quaternion .LookRotation(hit.normal));
+                Destroy(impactGO, 2f);
             }
         }
 
